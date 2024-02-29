c800ae71200ae1b6e96f7cf91129561a18a7a989
xexen id <sultonalazair@gmail.com>
2024-02-29 10:04:45 +0700

    Update AreaDemus

---
+++ AreaDemus/utils/decorators/language.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2021-2023 TeamUltroid.
+# Ultroid - UserBot.
+# UltroidUser - XexenId.
+#
+# This file is a part of https://github.com/teamultroid/ultroid.
+# Please read the GNU affero general license.
+# https://www.github.com/teamultroid/ultroid/blob/main/license.
+
+
+from strings import get_string
+
+from AreaDemus import app
+from AreaDemus.misc import SUDOERS
+from AreaDemus.utils.database import get_lang, is_maintenance
+
+
+def language(mystic):
+    async def wrapper(_, message, **kwargs):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    text=f"{app.mention} is under maintenance, visit <a href={SUPPORT_CHAT}>Support Chat</a> for knowing the reason.",
+                    disable_web_page_preview=True,
+                )
+        try:
+            await message.delete()
+        except:
+            pass
+
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+
+def languageCB(mystic):
+    async def wrapper(_, CallbackQuery, **kwargs):
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    f"{app.mention} is under maintenance, visit support chat for knowing the reason.",
+                    show_alert=True,
+                )
+        try:
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
+
+    return wrapper
+
+
+def LanguageStart(mystic):
+    async def wrapper(_, message, **kwargs):
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
