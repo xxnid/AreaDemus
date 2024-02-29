74fdf4026d777a7b5ebad02936c288bdda070a32
xexen id <sultonalazair@gmail.com>
2024-02-29 10:00:26 +0700

    Create sys.py

---
+++ AreaDemus/utils/sys.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2021-2023 TeamUltroid.
+# Ultroid - UserBot.
+# UltroidUser - XexenId.
+#
+# This file is a part of https://github.com/teamultroid/ultroid.
+# Please read the GNU affero general license.
+# https://www.github.com/teamultroid/ultroid/blob/main/license.
+
+
+import time
+
+import psutil
+
+from AreaDemus.misc import _boot_
+from AreaDemus.utils.formatters import get_readable_time
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - _boot_)
+    UP = f"{get_readable_time(bot_uptime)}"
+    CPU = f"{psutil.cpu_percent(interval=0.5)}%"
+    RAM = f"{psutil.virtual_memory().percent}%"
+    DISK = f"{psutil.disk_usage('/').percent}%"
+    return UP, CPU, RAM, DISK
