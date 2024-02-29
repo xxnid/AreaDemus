4a0e0c0e57209e4dadda284e590d019851fd5996
xexen id <sultonalazair@gmail.com>
2024-02-29 08:30:28 +0700

    Update 29.02.2024

---
+++ AreaDemus/platforms/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2021-2023 TeamUltroid.
+# Ultroid - UserBot.
+# UltroidUser - XexenId.
+#
+# This file is a part of https://github.com/teamultroid/ultroid.
+# Please read the GNU affero general license.
+# https://www.github.com/teamultroid/ultroid/blob/main/license.
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
