b6c15bd079e30d446da57c9c876e8fa654ede40d
xexen id <sultonalazair@gmail.com>
2024-02-29 10:16:11 +0700

    Update AreaDemus

---
+++ AreaDemus/utils/stream/autoclear.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2021-2023 TeamUltroid.
+# Ultroid - UserBot.
+# UltroidUser - XexenId.
+#
+# This file is a part of https://github.com/teamultroid/ultroid.
+# Please read the GNU affero general license.
+# https://www.github.com/teamultroid/ultroid/blob/main/license.
+
+
+import os
+
+from config import autoclean
+
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean.remove(rem)
+        count = autoclean.count(rem)
+        if count == 0:
+            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+                try:
+                    os.remove(rem)
+                except:
+                    pass
+    except:
+        pass
